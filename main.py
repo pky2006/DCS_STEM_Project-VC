bbbb3ae17dd26c8f4c2267ecb6a983bfb18b1cba
Alex <zhuangshikun@qq.com>
2022-12-23 13:27:51 +0800

    Improve the effiency of the program

--- main.py
+++ main.py
@@ -11,88 +11,64 @@ level_2=ques["lv_2"]
 level_3=ques["lv_3"]
 # structure: [{"Question":xxx, "Choice":["A","B",...],"Ans":xxx, "Elaborations":xxx},{...}]
 
+
+
+
 while True:
 	score = 0
 	correct = 0
 	wrong = 0
 	history = []
+	status = 1
+	ques_num = 1
 
 	print("\nWelcome to District Cooling System Q&A Game\n")
 	start=input("Do you want to start the game? (Press '0' to exit or press 'Enter' to start) :")
 	if start == '0':
 		sys.exit()
-	print("-"*10) # Divide diff. parts
+	print("-"*10,"\n") # Divide diff. parts
 
-	for a in range(0,len(level_1)):
-		print("Q: ",level_1[a]["Question"])
-		
-		for i in range(0,len(level_1[a]["Choice"])):
-			print(level_1[a]["Choice"][i])
+	for level in (level_1,level_2,level_3):
+		for a in range(0,len(level)):
+			print(f"Question: {ques_num} : {level[a]['Question']}")
+			ques_num += 1
 
-		while True:
-			ans=input("Ans: ").upper()
-			if ans == level_1[a]["Ans"]:
-				print("Well Done")
+			for i in range(0,len(level[a]["Choice"])):
+				print(level[a]["Choice"][i])
+
+			while True:
+				print("\n") 
+				ans=input("Your Ans: ").upper()
+
+				if status == 1 and ans in ("A","B"):
+					break
+				elif status == 2 and ans in ("A","B","C","D"):
+					break
+				elif status == 3 and ans in ("A","B","C","D","E"):
+					break
+				else:
+					print("Your input is wrong. Please input again.\n")
+
+			if ans == level[a]["Ans"]:
+				print("Well Done!")
 				score += 1
 				correct += 1
 
-			elif ans not in ("A","B"):
-				print("Please input again\n")
-				continue
-
 			else:
 				print("Good Try")
 				wrong += 1
+				print("\nThe Correct Ans: ",level[a]["Ans"])
+
 			history.append(ans)
-			break
 
+			if level[a]["Elaborations"] != "null":
+				print('\nElaborations: ',level[a]["Elaborations"],"\n\n")
 
-		print(history)
-		print("Score: ",score)
-		print('\nElaborations: ',level_1[a]["Elaborations"])
-		print("-"*20,"\n")
-		time.sleep(2)	# Wait 2 second for the user to read the elaborations
-	print("="*20,"\n")
+			print("Your Score: ",score)
+			print("-"*20,"\n")
+			time.sleep(2)	# Wait 2 second for the user to read the elaborations
 
-	print("Congratulation! You are level 2\n")
-
-	for a in range(0,len(level_2)):
-		print("Q: ",level_2[a]["Question"])
-		
-		for i in range(0,len(level_2[a]["Choice"])):
-			print(level_2[a]["Choice"][i])
-
-		while True:
-			ans=input("Ans: ").upper()
-			if ans == level_2[a]["Ans"]:
-				print("Well Done")
-				score += 2
-				correct += 1
-
-			elif ans not in ("A","B","C","D"):
-				print("Please input again\n")
-				continue
-
-			else:
-				print("Good Try")
-				wrong += 1
-			history.append(ans)
-			break
-
-
-		print(history)
-		print("Score: ", score)
-		print('\nElaborations: ',level_2[a]["Elaborations"])
-		print("-"*20,"\n")
-		time.sleep(2)	# Wait 2 second for the user to read the elaborations
-	print("="*20,"\n")
-
-	print("="*20,"\n\n")
-	print("The game is ended. \n Here is your mark:")
-	print("\t Total Score: ", score)
-	print("\tCorrect: " ,correct)
-	print("\tWrong: ",wrong)
-	print("\tHistory: ",history)
-	print("="*20,"\n\n")
-
-
+		status += 1
+		if status <= 3:
+			print(f"Congratulation! You are level {status}\n")
+			
